c4e243112a8d2e608f578fb015b6da8644bff18e
MGSimard <mgsimard.dev@gmail.com>
2025-06-20 22:28:25 -0400

    My configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,47 @@
-//  @ts-check
+import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
+import * as reactHooks from "eslint-plugin-react-hooks";
 
-import { tanstackConfig } from "@tanstack/eslint-config";
-
-export default [...tanstackConfig];
+export default tseslint.config(
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  reactHooks.configs.recommended,
+  {
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+      "react-compiler/react-compiler": "error",
+    },
+  }
+);
