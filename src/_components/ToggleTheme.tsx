876ff31aeb3374f52de43c632e2be340fe360d69
MGSimard <mgsimard.dev@gmail.com>
2025-06-21 07:33:15 -0400

    Theme select touchups

--- src/_components/ToggleTheme.tsx
+++ src/_components/ToggleTheme.tsx
@@ -1,4 +1,4 @@
-import { Moon, Sun } from "lucide-react";
+import { Check, Moon, Sun } from "lucide-react";
 
 import { Button } from "@/_components/ui/button";
 import {
@@ -7,24 +7,32 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/_components/ui/dropdown-menu";
-import { useTheme } from "@/_components/ThemeProvider";
+import { type Theme, useTheme } from "@/_components/ThemeProvider";
+
+const THEMES: Theme[] = ["light", "dark", "system"];
 
 export function ToggleTheme() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
+        <Button variant="ghost" size="icon" className="cursor-pointer">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+        {THEMES.map((themeOption) => (
+          <DropdownMenuItem
+            key={themeOption}
+            className="cursor-pointer capitalize"
+            onClick={() => setTheme(themeOption)}>
+            {themeOption}
+            {themeOption === theme && <Check className="ml-auto" />}
+          </DropdownMenuItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- src/_components/front-facing/Navbar.tsx
+++ src/_components/front-facing/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@tanstack/react-router";
 import { ToggleTheme } from "@/_components/ToggleTheme";
 import { Bolt } from "lucide-react";
+import { Button } from "../ui/button";
 
 const NAV_LINKS = [
   {
@@ -15,20 +16,25 @@ const NAV_LINKS = [
 
 export function Navbar() {
   return (
-    <header className="sticky top-0 w-full z-50 bg-background/40 px-6 py-4 flex justify-between items-center border-b backdrop-blur-sm">
-      <nav className="flex items-center gap-4">
-        <Link to="/">
-          <Bolt className="w-6 h-6 text-primary" />
-        </Link>
-        <ul className="flex items-center gap-4">
-          {NAV_LINKS.map((link) => (
-            <li key={link.label}>
-              <Link to={link.href}>{link.label}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <ToggleTheme />
+    <header className="sticky top-0 w-full z-50 border-b bg-background/40  backdrop-blur-sm">
+      <div className="max-w-8xl mx-auto flex justify-between items-center gap-4 px-6 py-4">
+        <nav className="flex items-center gap-4">
+          <Link to="/">
+            <Bolt className="w-6 h-6 text-primary" />
+          </Link>
+          <ul className="flex items-center gap-4">
+            {NAV_LINKS.map((link) => (
+              <li key={link.label}>
+                <Link to={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="flex items-center gap-4">
+          <ToggleTheme />
+          <Button variant="outline">Login</Button>
+        </div>
+      </div>
     </header>
   );
 }
