84d20a599e3c718aa79bd0b32ffd6025a4cd6bb5
MGSimard <mgsimard.dev@gmail.com>
2025-07-06 18:11:47 -0400

    Footer

--- src/_components/front-facing/Footer.tsx
+++ src/_components/front-facing/Footer.tsx
@@ -1,8 +1,40 @@
+import { Link } from "@tanstack/react-router";
+
+const links = [
+  {
+    title: "Home",
+    href: "#",
+  },
+  {
+    title: "Features",
+    href: "#",
+  },
+  {
+    title: "Pricing",
+    href: "#",
+  },
+  {
+    title: "About",
+    href: "#",
+  },
+];
+
 export function Footer() {
   return (
-    <footer className="border-t bg-background px-6 py-4">
-      <div className="max-w-8xl mx-auto">
-        <p>Footer</p>
+    <footer className="border-t bg-background py-12 px-6">
+      <div className="mx-auto max-w-8xl ">
+        <div className="flex flex-wrap justify-between gap-8">
+          <span className="text-muted-foreground order-last block text-center text-sm md:order-first">
+            © {new Date().getFullYear()} Company, All rights reserved
+          </span>
+          <div className="order-first flex flex-wrap justify-center gap-8 text-sm md:order-last">
+            {links.map((link, index) => (
+              <Link key={index} to={link.href} className="text-muted-foreground hover:text-primary block duration-150">
+                <span>{link.title}</span>
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </footer>
   );
