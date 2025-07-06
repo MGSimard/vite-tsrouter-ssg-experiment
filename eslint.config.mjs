f0379e5016fac7b43c84484c6ee12ed521dd1fd7
MGSimard <mgsimard.dev@gmail.com>
2025-07-05 22:42:26 -0400

    Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,14 @@ import tsParser from "@typescript-eslint/parser";
 import tseslint from "typescript-eslint";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import * as reactHooks from "eslint-plugin-react-hooks";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default tseslint.config(
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
   pluginQuery.configs["flat/recommended"],
   reactHooks.configs.recommended,
+  eslintConfigPrettier,
   {
     languageOptions: {
       parser: tsParser,
@@ -45,5 +47,5 @@ export default tseslint.config(
       ],
       "react-hooks/react-compiler": "error",
     },
-  }
+  },
 );
