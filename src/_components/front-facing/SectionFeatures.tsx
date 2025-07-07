c1333b5cfdab21201c6e80dea937d26495b603f2
MGSimard <mgsimard.dev@gmail.com>
2025-07-07 03:35:20 -0400

    Clean up

--- src/_components/front-facing/SectionFeatures.tsx
+++ src/_components/front-facing/SectionFeatures.tsx
@@ -1,8 +1,6 @@
 import { ChatterLogo } from "@/_components/front-facing/ChatterLogo";
-import { Activity, Map as MapIcon, MessageCircle, Shield, Users } from "lucide-react";
+import { Map as MapIcon, MessageCircle, Shield, Users } from "lucide-react";
 import DottedMap from "dotted-map";
-import { Area, AreaChart, CartesianGrid } from "recharts";
-import { type ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/_components/ui/chart";
 import { Card, CardContent } from "@/_components/ui/card";
 
 export function SectionFeatures() {
@@ -334,70 +332,5 @@ const Map = () => {
         <circle key={index} cx={point.x} cy={point.y} r={svgOptions.radius} fill={svgOptions.color} />
       ))}
     </svg>
-  );
-};
-
-const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "#E02B20",
-  },
-  mobile: {
-    label: "Mobile",
-    color: "#bc6560",
-  },
-} satisfies ChartConfig;
-
-const chartData = [
-  { month: "May", desktop: 56, mobile: 224 },
-  { month: "June", desktop: 56, mobile: 224 },
-  { month: "January", desktop: 126, mobile: 252 },
-  { month: "February", desktop: 205, mobile: 410 },
-  { month: "March", desktop: 200, mobile: 126 },
-  { month: "April", desktop: 400, mobile: 800 },
-];
-
-const MonitoringChart = () => {
-  return (
-    <ChartContainer className="h-120 aspect-auto md:h-96" config={chartConfig}>
-      <AreaChart
-        accessibilityLayer
-        data={chartData}
-        margin={{
-          left: 0,
-          right: 0,
-        }}>
-        <defs>
-          <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="0%" stopColor="var(--color-desktop)" stopOpacity={0.8} />
-            <stop offset="55%" stopColor="var(--color-desktop)" stopOpacity={0.1} />
-          </linearGradient>
-          <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="0%" stopColor="var(--color-mobile)" stopOpacity={0.8} />
-            <stop offset="55%" stopColor="var(--color-mobile)" stopOpacity={0.1} />
-          </linearGradient>
-        </defs>
-        <CartesianGrid vertical={false} />
-        <ChartTooltip active cursor={false} content={<ChartTooltipContent className="dark:bg-muted" />} />
-        <Area
-          strokeWidth={2}
-          dataKey="mobile"
-          type="stepBefore"
-          fill="url(#fillMobile)"
-          fillOpacity={0.1}
-          stroke="var(--color-mobile)"
-          stackId="a"
-        />
-        <Area
-          strokeWidth={2}
-          dataKey="desktop"
-          type="stepBefore"
-          fill="url(#fillDesktop)"
-          fillOpacity={0.1}
-          stroke="var(--color-desktop)"
-          stackId="a"
-        />
-      </AreaChart>
-    </ChartContainer>
   );
 };
